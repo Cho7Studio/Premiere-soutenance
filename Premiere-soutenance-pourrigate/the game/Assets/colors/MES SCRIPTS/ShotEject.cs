e9c02145f44a285a0a70441fe2c385625ca08014
CyberGenesys <dark.genesys@free.fr>
2016-04-19 21:44:05 +0200

    Test

---
+++ Premiere-soutenance-pourrigate/the game/Assets/colors/MES SCRIPTS/ShotEject.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ShotEject : MonoBehaviour {
+
+	public Rigidbody[] bulletCasing;
+	public int ejectSpeed = 50;
+	public float fireRate = 0.5f;
+	public int munition = 30;
+	public int maxMunition = 100;
+	public GUIStyle InstructionBoxSkin;
+
+	private Rigidbody bullet;
+	private float nextFire = 0.0f;
+	private bool fullAuto = false;
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButton("Fire1") && (munition > 0) && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			bullet = (UnityEngine.Rigidbody) Instantiate (bulletCasing[(int)Random.Range (0f, (float)bulletCasing.Length)], transform.position, transform.rotation);
+			munition--;
+			bullet.velocity = transform.TransformDirection (Vector3.left * ejectSpeed);
+		}
+
+		if (Input.GetButtonDown ("Fire2")) 
+		{
+			fullAuto = !fullAuto;
+		}
+
+		if (fullAuto) 
+		{
+			fireRate = 0.1f;
+		} 
+		else 
+		{
+			fireRate = 0.5f;
+		}
+
+		if (munition > maxMunition) 
+		{
+			munition = maxMunition;
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(Screen.width - 155, 10, 150, 30), "Cadeaux : " + munition, InstructionBoxSkin);
+	}
+
+	public void MoreMunition()
+	{
+		munition += 10;
+	}
+}
---
+++ Premiere-soutenance-pourrigate/the game/Assets/colors/MES SCRIPTS/Weapon.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var TheDammage : int = 33;
+private var Distance : float;
+var MaxDistance : float = 1.5;
+var DammageDelay : float = 0.6;
+
+var hitsound : AudioClip;
+
+function Update () 
+{
+	if(Input.GetButtonDown("Fire1"))
+	{
+		GetComponent.<AudioSource>().PlayOneShot(hitsound);
+		AttackDammage();
+	}
+
+}
+
+
+function AttackDammage()
+{
+	yield WaitForSeconds(DammageDelay);
+	var hit : RaycastHit;
+	var ray = Camera.main.ScreenPointToRay(Vector3(Screen.width/2, Screen.height/2, 0));
+
+	if(Physics.Raycast (ray,hit))
+	{
+		Distance = hit.distance;
+		if(Distance < MaxDistance)
+		{
+			hit.transform.SendMessage("ApplyDammage", TheDammage, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+}
