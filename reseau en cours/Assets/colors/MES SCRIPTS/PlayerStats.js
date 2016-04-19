b38167d9f826e1e9d0495eebbacb383e043ee757
Heldhy <danae.marmai@gmail.com>
2016-04-19 22:26:15 +0200

    test

---
+++ reseau en cours/Assets/colors/MES SCRIPTS/PlayerStats.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var Health : float = 100.0;
+var maxHP : float = 100.0;
+
+var HpTexture : Texture2D;
+var HpLenght : float;
+var hitsound : AudioClip;
+
+function Start()
+{
+	Screen.lockCursor = true;
+}
+
+function OnGUI()
+{
+	if(Health > 0)
+	{
+		GUI.DrawTexture(Rect(10,10, HpLenght, 10), HpTexture);
+	}
+}
+
+function Update()
+{
+	HpLenght = (Health/maxHP) * 100;
+	if(Health <=0)
+	{
+		Health = 0;
+	}
+}
+
+function ApplyDammage (TheDammage : int)
+{
+	Health = Health - TheDammage;
+	GetComponent.<AudioSource>().PlayOneShot(hitsound);
+	if(Health <= 0)
+	{
+		Dead();
+	}
+
+}
+
+function Dead() 
+{
+	Application.LoadLevel("GameOver");
+}
