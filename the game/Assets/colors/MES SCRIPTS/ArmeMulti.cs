99c843547e0c459a2a64fe3856954e6dd5e4e269
Heldhy <danae.marmai@gmail.com>
2016-04-27 06:38:42 +0200

    generation d'objets par le serveur

--- the game/Assets/colors/MES SCRIPTS/ArmeMulti.cs
+++ the game/Assets/colors/MES SCRIPTS/ArmeMulti.cs
@@ -1,21 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.ComponentModel.Design;
+using UnityEngine.Networking;
 
 
-public class ArmeMulti : MonoBehaviour {
+public class ArmeMulti : NetworkBehaviour {
 
-	public Rigidbody[] bulletCasing;
-	public int ejectSpeed = 50;
+	public GameObject[] bulletCasing;
 	public float fireRate = 1f;
 
+	private Transform Mytransform;
 	private GameObject arme;
 	private Rigidbody bullet;
 	private float nextFire = 0.0f;
 
+	[Command]
+	public void CmdShoot()
+	{
+		 
+		GameObject obj = (GameObject)Instantiate (bulletCasing [(int)Random.Range (0f, (float)bulletCasing.Length)], Mytransform.position, Mytransform.rotation);
+		BulletMulti bullet = obj.GetComponent<BulletMulti> ();
+		Destroy (obj,5.0f);
+		NetworkServer.Spawn (obj);
+	}
 
 	void Start()
 	{
 		arme = GameObject.FindGameObjectWithTag ("armemulti");
+		Transform[] ts = gameObject.GetComponentsInChildren<Transform> ();
+		foreach (Transform t in ts) 
+		{
+			if (t.name == "FirstPersonCharacter") {
+				Transform[] poet = t.GetComponentsInChildren<Transform> ();
+				foreach (Transform e in poet) 
+				{
+					if (e.name == "Weapon") {
+						Transform[] hey = e.GetComponentsInChildren<Transform> ();
+						foreach (Transform v in hey) {
+							if (v.name == "armepourrimulti 1 1") {
+								Transform[] enfin = v.GetComponentsInChildren<Transform> ();
+								foreach (Transform yeah in enfin) {
+									if (yeah.name == "Canon") {
+										Mytransform = yeah.transform;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 	void Update () 
@@ -26,10 +60,14 @@ public class ArmeMulti : MonoBehaviour {
 		}
 		if (Input.GetButton("Fire1") && Time.time > nextFire) 
 		{
+			CmdShoot ();
 			nextFire = Time.time + fireRate;
-			bullet = (UnityEngine.Rigidbody) Instantiate (bulletCasing[(int)Random.Range (0f, (float)bulletCasing.Length)], transform.position, transform.rotation);
 			arme.GetComponent<Animation>().PlayQueued ("pourriMulti");
-			bullet.velocity = transform.TransformDirection (Vector3.left * ejectSpeed);
 		}
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect(Screen.width/2,Screen.height/2,200,200), "+");
 	}
 }
---
+++ the game/Assets/colors/MES SCRIPTS/BulletMulti.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class BulletMulti : NetworkBehaviour {
+
+	public float moveSpeed = 1.0f;
+	public Vector3 velocity;
+	public int ejectSpeed = 50;
+	Rigidbody bullet;
+
+	void Start()
+	{
+		bullet = gameObject.GetComponent<Rigidbody> ();
+		bullet.velocity = transform.TransformDirection(Vector3.left * ejectSpeed);
+	}
+}
